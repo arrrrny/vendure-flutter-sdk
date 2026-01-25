7e764d45c4de4f9d25e88ab199ee3a2c5e7cb3ec
ARRRRNY <ahmettok@gmail.com>
2026-01-25 23:26:50 +0300

    removed unused fragment import

--- lib/src/fragments/customer_fragment.dart
+++ lib/src/fragments/customer_fragment.dart
@@ -1,6 +1,3 @@
-import 'package:vendure/src/fragments/order_fragment.dart';
-import 'package:vendure/src/fragments/shared_fragment.dart';
-
 const String customerFragment = r'''
 fragment Customer on Customer {
   __typename
